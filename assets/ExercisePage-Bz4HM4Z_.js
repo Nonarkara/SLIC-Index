d9374766edfd38c42d720b31d5b01a3f0a96f94c
Non Arkara <nonarkara@Nons-MacBook-Pro.local>
2026-04-13 18:41:30 +0700

    Final Alpha: user-approved top 10 across 6 regions

---
+++ assets/ExercisePage-Bz4HM4Z_.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-BfcU4Ujz.js";function s({onNavigate:r}){return e.useEffect(()=>{r("/rankings")},[r]),null}export{s as default};
--- assets/ExercisePage-C3d57N6n.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-Dro_zd8w.js";function s({onNavigate:r}){return e.useEffect(()=>{r("/rankings")},[r]),null}export{s as default};
