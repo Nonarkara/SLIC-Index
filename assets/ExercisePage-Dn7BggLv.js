6fb1c1811c5c77fc96854ca38940271ae48cf93e
Non Arkara <nonsmartcity@gmail.com>
2026-05-04 12:40:20 +0700

    Updates

---
+++ assets/ExercisePage-Dn7BggLv.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-Cnd4_oix.js";function s({onNavigate:r}){return e.useEffect(()=>{r("/rankings")},[r]),null}export{s as default};
--- assets/ExercisePage-uZlwiqSz.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-B9puimYP.js";function s({onNavigate:r}){return e.useEffect(()=>{r("/rankings")},[r]),null}export{s as default};
